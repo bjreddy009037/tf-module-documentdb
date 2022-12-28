6d92063f8c4d07a53dc44389775a2740a99e27a2
bjreddy009037 <bjreddy246@gmail.com>
2022-12-28 21:09:21 +0000

    terraform

---
+++ data.tf
@@ -0,0 +1,10 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "terraformforme"
+    key    = "vpc/${var.ENV}/${var.COMPONENT}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "ENGINE" {}
+variable "ENGINE_VERSION" {}
+variable "NODE_TYPE" {}
+variable "CLUSTER_ID" {}
+variable "ENV" {}
+variable "COMPONENT" {}
+variable "NODE_COUNT" {}
+variable "FAMILY" {}
